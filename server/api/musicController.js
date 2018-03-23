66a91f21384fe51ae109d2fa7ac4031bb808fdf4
Timothy Booser <tbooser@utexas.edu>
2018-03-23 16:14:02 -0500

    More converting indentation to spaces

--- server/api/musicController.js
+++ server/api/musicController.js
@@ -7,9 +7,7 @@
 
 	exports.getMusicById = (req, res) => {
 		console.log('Getting music by ID')
-		// console.log('request', req)
 		db.getRelease(req.body, function(err, data){
-    	console.log(data);
     	res.send(data)
   	});
 	}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import discogsImage from '../images/discogs-logo.png'
 
 export default function Header() {
-	return (
-		<div className='container'>
-			<div className='row'>
-				<div className='header'>
-					Tim's
-					<div className='header-image'>
-						<img alt='header-discogs' src={discogsImage}/>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='header'>
+          Tim's
+          <div className='header-image'>
+            <img alt='header-discogs' src={discogsImage}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
