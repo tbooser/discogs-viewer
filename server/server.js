f030c4ceab5596acb5935cb4e52a42c1fd4f9a80
Timothy Booser <tbooser@utexas.edu>
2018-04-04 15:44:09 -0500

    Added production

--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ const express = require("express");
 const app = express();
 const musicController = require("./api/musicController");
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+}
+
 app.get("/music", musicController.getMusicByCollection);
 
 const server = app.listen(3001, () => {
-  console.log("Listening on port " + server.address().port);
+	console.log("Listening on port " + server.address().port);
 });
