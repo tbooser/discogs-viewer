9a824c6cd8e0cf4d4308bc3a839d90585abc6461
Tim <timothy.booser@time.com>
2022-06-14 21:27:36 -0400

    tboos username

--- app/api/musicController.ts
+++ app/api/musicController.ts
@@ -9,7 +9,7 @@ const Discogs = require('disconnect').Client;
 const dis = new Discogs(config);
 const discogsCollection = dis.user().collection();
 const discogsWantlist = dis.user().wantlist();
-const username = 'booserk';
+const username = 'tboos';
 
 interface RecordResponseTypes {
   id: number;
